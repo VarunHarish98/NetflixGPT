a963a056e0611d367001ed96c998771bf7a30133
VarunHarish <varunharish98@gmail.com>
2023-11-14 13:50:32 -0500

    Integrated TMDB API

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { nowPlayingAPI } from "../APIs/api";
 
 const Browse = () => {
+  const getNowPlayingAPI = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', nowPlayingAPI)
+    const json = await data?.json();
+    console.log(json);
+  }
+  useEffect(() => {
+    getNowPlayingAPI()
+  }, []); //Call only once;
   return (
     <div>
       <Header />
