1f7e71c07be2e2da1b7e44c7797d65ca4850de7c
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2023-11-27 20:43:19 -0500

    Fixed the video title and overview not displaying issue

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -3,19 +3,26 @@ import { useSelector } from "react-redux";
 import useMovieTrailer from "../customHooks/useMovieTrailer";
 
 const VideoBackground = (movieId) => {
-  const trailerVideoKey = useSelector((store) => store.movies?.trailerDataKey);
-  console.log(trailerVideoKey);
+  const trailerVideoKey = useSelector((store) => {
+    console.log("Redux Store:", store); // Log the entire store to inspect its structure
+    return store.movies?.trailerDataKey;
+  });
+
+  console.log("Trailer Video Key:", trailerVideoKey);
+
   useMovieTrailer(movieId?.id);
 
   let url = `https://www.youtube.com/embed/${trailerVideoKey}`;
-  
+  console.log("Video URL:", url);
+
   return (
-    <div className="w-screen h-screen overflow-hidden relative">
+    <div className="w-screen h-screen">
       <iframe
-        className="w-full h-full aspect-w-16 aspect-h-9 transform scale-150 relative"
-        src={`${url}?autoplay=1&mute=1&controls=0&loop=1&frameborder=0&autoplay&modestbranding=1=1&vq=hd2360`}
+        className="h-full w-full bg-gray-900 flex items-center justify-center overflow"
+        src={`${url}?autoplay=1&mute=1&controls=0`}
         allowFullScreen
         allow="autoplay; clipboard-write; encrypted-media; web-share"
+        title="Video Player"
       ></iframe>
     </div>
   );
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="absolute top-0 left-0 right-0 p-8 text-white bg-gradient-to-r from-black aspect-video">
+    <div className="absolute top-1/4 py-10 mt-30 left-0 right-0 p-8 text-white bg-gradient-to-r from-black-600 to-transparent aspect-video">
       <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="py-6 text-lg">{overview}</p>
+      <p className="py-6 text-lg w-1/4 font-semibold">{overview}</p>
       <div className="py-3">
         <button className="text-xl bg-white text-black p-4 px-12 rounded-lg hover:opacity-80">
           ▶ Play
