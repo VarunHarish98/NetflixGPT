f6603278c07a63ef4b10bac4f5674139980d8ef9
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2023-12-02 20:01:01 -0500

    Updated CSS to show MovieList

--- src/Components/Secondary.js
+++ src/Components/Secondary.js
@@ -6,7 +6,7 @@ const Secondary = () => {
   const movies = useSelector((store) => store.movies);
   return (
     <div className="bg-black">
-      <div className="relative -mt-52 z-20 ">
+      <div className="-mt-52 relative z-20 ">
         {<MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />}
         {<MovieList title={"Popular"} movies={movies?.popularMovies} />}
         {<MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />}
