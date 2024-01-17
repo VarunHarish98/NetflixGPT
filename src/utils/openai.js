31c0346a270680ac23bb2e8f57eb5fb3edf93a9b
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2024-01-17 17:15:25 -0500

    Removed Unnecessary Loggers

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 // import { OPEN_AI_KEY } from "../constants/constants";
-console.log(process.env)
+// console.log(process.env)
 const openai = new OpenAI({
   apiKey: process.env["REACT_APP_OPEN_AI_KEY"], // defaults to process.env["OPENAI_API_KEY"]
   dangerouslyAllowBrowser: true
