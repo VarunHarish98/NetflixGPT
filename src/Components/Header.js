a433e6b70a978e8998e94eba07f1e6acdb0e0572
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2024-01-17 14:12:36 -0500

    Added Header Responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,18 +35,18 @@ const Header = () => {
   };
 
   return (
-    <div className="relative h-screen w-screen z-10">
+    <div className="bg-gradient-to-r from black-700 flex relative w-screen z-10">
       <img
-        className="w-44"
+        className="top-0 left-0 absolute w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="Logo"
       />
 
       {user && (
-        <div className="flex p-4 z-10">
+        <div className="flex absolute top-0 right-0 p-4 z-10">
           {showGPTButton && (
             <select
-              className="p-2 m-2 bg-red-600 text-white rounded-lg"
+              className="p-2 m-2 bg-red-600 text-white rounded-lg max-w-xs md:max-w-md w-auto"
               onChange={handleOnChange}
             >
               {LANG_SUPPORTED.map((lang) => (
@@ -58,7 +58,7 @@ const Header = () => {
           )}
           <button
             onClick={handleGPTSearch}
-            className="p-4 m-2 bg-red-600 rounded-lg text-white"
+            className="p-4 m-2 bg-red-600 rounded-lg text-white w-auto"
           >
             {showGPTButton ? GPTHomepage : GPTButtonName}
           </button>
@@ -67,7 +67,10 @@ const Header = () => {
             className="w-14 h-14"
             src="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp"
           />
-          <button onClick={handleSignout} className="font-bold text-white">
+          <button
+            onClick={handleSignout}
+            className="top-0 right-0 font-bold text-white"
+          >
             Sign-Out
           </button>
         </div>
