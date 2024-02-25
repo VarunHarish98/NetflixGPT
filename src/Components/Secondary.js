6460a96f1478598cc71e9f5205905daf78eab890
Varun Hosadurga Harish <varun.hosadurgaharish@stonybrook.edu>
2024-02-25 03:29:34 -0500

    CSS Fix

--- src/Components/Secondary.js
+++ src/Components/Secondary.js
@@ -6,7 +6,7 @@ const Secondary = () => {
   const movies = useSelector((store) => store.movies);
   return (
     <div className="bg-black">
-      <div className="-mt-52 relative z-20 ">
+      <div className=" relative z-20 ">
         {<MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />}
         {<MovieList title={"Popular"} movies={movies?.popularMovies} />}
         {<MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />}
