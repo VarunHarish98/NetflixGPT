a2fad3fadec84f0fd18f1b79bcf66ab6f31f26b4
VarunHarish98 <varunharish98@gmail.com>
2024-03-01 18:22:59 -0500

    Updated Modal Logic and display dynamic Modal Cards

--- src/Components/ModalCard.js
+++ src/Components/ModalCard.js
@@ -2,10 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { tmdbURL } from "../constants/constants";
 
-const ModalCard = () => {
+const ModalCard = ({ id }) => {
   const movieDetails = useSelector((store) => store.movies);
 
-  let url = tmdbURL + "/pA4sNvcohTNPx3AhEEeIu8gSt7h.jpg";
+  let movieList = movieDetails["nowPlayingMovies"]?.filter((item) => item["id"] === id) || movieDetails["popularMovies"]?.filter((item) => item["id"] === id) || movieDetails["topRatedMovies"]?.filter((item) => item["id"] === id) || movieDetails["upcomingMovies"]?.filter((item) => item["id"] === id)
+  // console.log(movieList[0])
+  let { title, overview, poster_path } = movieList[0]
+  // console.log(poster_path)
+  let url = tmdbURL + poster_path;
+  // console.log(url)
 
   return (
     <div>
@@ -14,12 +19,12 @@ const ModalCard = () => {
           <img src={url} className="w-66 h-40" />
           <div className="w-30 h-56">
             <h1 className="text-white text-lg font-bold mb-2">
-              Title - {movieDetails.title}
+              Title - {title}
             </h1>
             <p className="text-white ">
               {" "}
-              <div className="font-bold">Description</div> <br></br>
-              {movieDetails.overview}
+              <div className="font-bold">Description</div>
+              {overview}
             </p>
           </div>
         </div>
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,17 +1,26 @@
+// MovieCard.js
 import React, { useState } from "react";
 import { tmdbURL } from "../constants/constants";
 import Modal from "./Modal";
 
-export const MovieCard = ({id, moviesPoster }) => {
-  const [isModal, setModal] = useState(false)
+export const MovieCard = ({ id, moviesPoster, onClick }) => {
+  const [isModal, setModal] = useState(false);
+
   const toggleModal = () => {
-    setModal(!isModal)
-  }
+    setModal(!isModal);
+  };
+
   return (
     <div className="w-52 pr-1">
-      {/* {console.log(moviesPoster)} */}
-      <img src={tmdbURL + moviesPoster} alt="Movie Card Text" onClick={() => toggleModal()}/>
-      {isModal && <Modal id={id} onClose={toggleModal} visible={isModal}/>}
+      <img
+        src={tmdbURL + moviesPoster}
+        alt="Movie Card Text"
+        onClick={() => {
+          onClick(id);
+          toggleModal();
+        }}
+      />
+      {isModal && <Modal id={id} onClose={toggleModal} visible={isModal} />}
     </div>
-  );    
+  );
 };
