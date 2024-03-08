e973602116da5fed6c857336f82225a1e493e5eb
VarunHarish98 <varunharish98@gmail.com>
2024-03-08 15:44:45 -0500

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Body from "./Components/Body";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react"
 import appStore from "./redux/appStore";
 function App() {
   return (
     <Provider store={appStore}>
+      <Analytics />
       {" "}
       <Body />
     </Provider>
